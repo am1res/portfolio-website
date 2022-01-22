6f8068853cc5fa5af4dcca14749889896b0824a5
Amir <saulebekovamir@gmail.com>
2022-01-22 16:10:51 +0600

    menu

--- js/script.js
+++ js/script.js
@@ -46,7 +46,14 @@ if (menuLinks.length > 0) {
 		if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
 			const gotoBlock = document.querySelector(menuLink.dataset.goto);
 			const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset;
-		
+			
+			if (iconMenu.classList.contains("_active")){
+				document.body.classList.remove("_lock");
+				iconMenu.classList.remove("_active");
+				menuBody.classList.remove("_active");
+			}
+
+
 			window.scrollTo({
 				top: gotoBlockValue,
 				behavior:"smooth"
@@ -54,4 +61,16 @@ if (menuLinks.length > 0) {
 			e.preventDefault();
 		}
 	}
+}
+
+//burger menu
+const iconMenu = document.querySelector(".menu_icon");
+const menuBody = document.querySelector(".menu");
+if (iconMenu){
+	
+	iconMenu.addEventListener("click", function(e) {
+		document.body.classList.toggle("_lock");
+		iconMenu.classList.toggle("_active");
+		menuBody.classList.toggle("_active");
+	});
 }
